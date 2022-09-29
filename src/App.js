56c24f97177949cccde8d60917db4dd330988c12
Git_TJ <98149148+GeoffreyGichaga@users.noreply.github.com>
2022-09-29 06:17:40 +0300

    add pages menu && Safricom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
-
+import Menu from './pages/Menu'
+import Sim from './pages/Sim';
 function App() {
   return (
     <Routes>
       <Route path='/' element={<Home/>}/>
+      <Route path='/menu' element={<Menu/>}/>
+      <Route path='/safaricom' element={<Sim/>}/>
     </Routes>
   );
 }

8053f8b9fd675b4763911865891219f87259c270
Jeff <geofreygichaga@gmail.com>
2022-11-17 16:36:59 +0300

    add navbar with time and network icons

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,8 +5,23 @@ import { useNavigate } from 'react-router'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import '../styling/home.css'
+
+
+import network1 from '../Assets/mobiledata.png'
+import network2 from '../Assets/networksignal.png'
+
+
+
+
+
 const Home = () => {
   const navigate = useNavigate()
+
+
+  const time = new Date()
+   const currentTime = time.toLocaleTimeString()
+   const exactTime = currentTime.slice(0,-6)
+
 
   
 
@@ -14,22 +29,40 @@ const Home = () => {
     <>
     <h1>Home page</h1>
     {/* <h3 onClick={()=> navigate('/menu')} >menu</h3> */}
-    <Container className='d-flex justify-content-center'>
-      <Row>
-        <Col className='phone'>
-          <h3>Hello</h3>
-          <h3>Hello</h3>
-          <h3>Hello</h3>
-          <h3>Hello</h3>
-          <h3>Hello</h3>
-          <h3>Hello</h3>
-          <h3>Hello</h3>
-          <h3>Hello</h3>
+    <Container className='cont'>
+      <Row  className='phonerow'>
+        {/* NavBar */}
+        <Row>
+          <div className='nav d-flex justify-content-between'>
+            {/* Time */}
+          
+            <p className='time ps-1'>{exactTime}</p>
+          
+            <div>
+            <img className='net1' src={network1} alt='net'/>
+            <img className='net2' src={network2} alt='net2'/>
+            </div>
+
+  
+          </div>
+        </Row>
+
+
 
         
-        </Col>
-      </Row>
 
+        {/* Weather Icon  */}
+        <Row>
+
+        </Row>
+
+
+
+
+
+      </Row>
+      
+    
     </Container>
 
     </>

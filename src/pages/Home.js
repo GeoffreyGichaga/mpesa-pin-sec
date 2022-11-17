2893dfcb00c27704c9f5c7e64fbddcafcf9cdb95
Jeff <geofreygichaga@gmail.com>
2022-11-17 16:49:44 +0300

    add weather icon & text

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,7 +10,7 @@ import '../styling/home.css'
 import network1 from '../Assets/mobiledata.png'
 import network2 from '../Assets/networksignal.png'
 
-
+import cloudy from '../Assets/cloudy.png'
 
 
 
@@ -19,8 +19,11 @@ const Home = () => {
 
 
   const time = new Date()
-   const currentTime = time.toLocaleTimeString()
-   const exactTime = currentTime.slice(0,-6)
+  const currentTime = time.toLocaleTimeString()
+  const exactTime = currentTime.slice(0,-6)
+
+  const nairobiWeather ="https://www.google.com/search?q=weather+nairobi&oq=weather+&aqs=chrome.3.69i57j35i39j0i131i433i512l3j0i131i433i457i512j69i60l2.4133j0j4&sourceid=chrome&ie=UTF-8"
+
 
 
   
@@ -49,11 +52,22 @@ const Home = () => {
 
 
 
-        
+
 
         {/* Weather Icon  */}
-        <Row>
+        <Row className='weatherrow'>
+          <div className='ms-5'>
+            <img className='cloudy' src={cloudy} alt='weather'/>
 
+          </div>
+
+          <a href={nairobiWeather} className='weathertext ms-3'  target='blank'>Tap for weather Info</a>
+        </Row>
+
+
+        {/* Google Search Bar */}
+        <Row>
+          
         </Row>
 
 

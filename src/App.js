85d4a559c83b00c42b54cc2d67a69fcfd73c822e
Jeff <geofreygichaga@gmail.com>
2023-01-18 17:31:52 +0300

    add Amount & Pin components

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ import Toolkit from './pages/Toolkit';
 import SafMenu from './pages/SafMenu';
 import MpesaMenu from './pages/MpesaMenu';
 import EnterNo from './pages/EnterNO';
-
+import Amount from './pages/Amount';
+import Pin from './pages/Pin'
 
 
 function App() {
@@ -18,6 +19,10 @@ function App() {
       <Route path='/safmenu' element={<SafMenu/>}/>
       <Route path='/mpesamenu' element={<MpesaMenu/>}/>
       <Route path='/enternumber' element={<EnterNo/>}/>
+      <Route path='/amount' element={<Amount/>}/>
+      <Route path='/pin' element={<Pin/>}/>
+
+
       
 
       
---
+++ src/pages/Pin.js
@@ -0,0 +1,62 @@
+import React from 'react'
+import Container from 'react-bootstrap/esm/Container'
+import Row from 'react-bootstrap/esm/Row'
+import Col from 'react-bootstrap/Col'
+
+import '../styling/mpesamenu.css'
+import dots from "../Assets/dots.png"
+import '../styling/EnterNo.css'
+
+import Form from 'react-bootstrap/Form'
+import InputGroup from 'react-bootstrap/InputGroup'
+import Button from 'react-bootstrap/Button'
+
+
+const Pin = () => {
+
+  return (
+    <>
+      <h1>Mpesa menu</h1>
+
+      <Container>
+      <Row  className='toolkitrow'>
+        {/* NavBar */}
+
+        <Row>
+          <Col className='d-flex justify-content-start'>
+            <h3 className='titles mt-2 mb-5'>Safaricom</h3>
+          </Col>
+
+        
+
+        
+        </Row>
+
+
+        <Row className='mpesarow2'>
+          <p className='enter-title text-center'>Enter Pin</p>
+          <Form.Label className='digits-title' htmlFor="basic-url">Digits(0-9,*,#,+) 4</Form.Label>
+          <InputGroup className="mb-3">
+          
+            <Form.Control id="input-space" type='number' aria-describedby="basic-addon3" />
+          </InputGroup> 
+          <div className='d-flex justify-content-center'>
+            <Button id='ok-btn'>OK</Button>
+          </div>
+            
+
+
+
+
+       
+        </Row>
+
+        
+
+      </Row>
+      </Container>
+      </>
+  )
+}
+
+export default Pin
